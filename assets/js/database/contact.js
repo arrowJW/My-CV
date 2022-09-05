9d697e2607d73caaacc8bb4241f203abe394edd1
arrowJW <110177773+arrowJW@users.noreply.github.com>
2022-09-05 13:41:59 +0000

    Minor fix
    Fixed mismatch between social media links

--- assets/js/database/contact.js
+++ assets/js/database/contact.js
@@ -7,8 +7,8 @@ function contact(iconS,iconL,title,link) {
 
 const CONTACTS = [
   new contact("fas fa-envelope", "fas fa-2x fa-envelope", "Email", "mailto:meetme.jw_arrow@erine.email"),
-  new contact("fa-brands fa-linkedin", "fa-brands fa-2x fa-linkedin", "LinkedIn", "https://www.linkedin.com/in/arrowjames"),
-  new contact("fa-brands fa-vimeo", "fa-brands fa-2x fa-vimeo", "Vimeo", "https://uk.linkedin.com/in/arrowjames"),
+  new contact("fa-brands fa-linkedin", "fa-brands fa-2x fa-linkedin", "LinkedIn", "https://uk.linkedin.com/in/arrowjames"),
+  new contact("fa-brands fa-vimeo", "fa-brands fa-2x fa-vimeo", "Vimeo", "https://vimeo.com/user71357499"),
   new contact("fa-brands fa-deviantart", "fa-brands fa-2x fa-deviantart", "DeviantArt", "https://www.deviantart.com/coal-sekitan")
 ];
 
